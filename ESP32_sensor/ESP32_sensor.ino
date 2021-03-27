179393d58190d9b3c78e72af804c4c88e82c033c
frankyboy_89@hotmail.com <frankyboy_89@hotmail.com>
2021-03-27 18:03:11 -0400

    initial MQTT connection

--- ESP32_sensor/ESP32_sensor.ino
+++ ESP32_sensor/ESP32_sensor.ino
@@ -1,9 +1,14 @@
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <WiFi.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DHTTYPE DHT22
+#define DHTTYPE     DHT22
+#define DHTPIN      14
 
-#define DHTPIN 14
+
+
 int PIN_LED = 2;
 int PIN_Moisture = 35;
 int PIN_Light = 15;
@@ -15,18 +20,58 @@ const int Light_Indirect = 220; // Sunny indirect 200=300
 const int LightMin_Night = 4000;
 
 
-
 DHT dht(DHTPIN, DHTTYPE);
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
+
+void MQTT_connect();
+
 
 void setup() {
   Serial.begin(9600);
+  // wait for serial monitor to open
+  while(! Serial);
+  // Connection to WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+//  // connect to io.adafruit.com
+//  Serial.print("Connecting to Adafruit IO");
+//  io.connect();
+//  // wait for a connection
+//  while(io.status() < AIO_CONNECTED) {
+//    Serial.print(".");
+//    delay(500);
+//  }
+//  // we are connected
+//  Serial.println();
+//  Serial.println(io.statusText());
+  
+  // Defining sensor pins  
   pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_Moisture, INPUT);
   pinMode(PIN_Light, INPUT);
   dht.begin();
+
 }
 
 void  loop() {
+  MQTT_connect();
+  
   // PIN_LED Bliking (Test)
   digitalWrite(PIN_LED, HIGH);
   delay(100);
@@ -65,11 +110,32 @@ void  loop() {
   delay(1000);
 
   // Light Sensor
-float  light_sensor = analogRead(PIN_Light);
+  float  light_sensor = analogRead(PIN_Light);
   Serial.print("Light value: ");
   Serial.println(light_sensor);
+
+  //Sending data to Adafruit IO
+  temperature.publish(air_tempC);
 }
 
 
-
-
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+ 
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+ 
+  Serial.print("Connecting to MQTT... ");
+ 
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+  }
+  Serial.println("MQTT Connected!");
+}
